f1621365770ebc4e945e920db82996a299591bc4
ChrisAchinga <achinga.chris@gmail.com>
2020-08-24 00:48:56 +0300

    /

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('article.urls')),
-    path('acount/', include('django.contrib.auth.urls')),
-    path('acount/', include('account.urls')),
-]
